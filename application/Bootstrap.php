fa9edf9a6df853fb62d5fcf4ca3f5a245fe30dcc
Rob Zienert <rob@robzienert.com>
2010-06-27 12:23:30 -0400

    initial commit

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,56 @@
 <?php
-
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
+    /**
+     * The first method of handling routes and my personal favorite.
+     *
+     * Using static routes allows us to add pages right into the router. It
+     * results in pleasant-looking URLs and really easy links using the url
+     * view helper. On the downside, you have to manage a list of pages
+     * somewhere... like here in the bootstrap, in the application.ini.
+     */
+    protected function _initMvcStaticRoutes()
+    {
+        $this->bootstrap('frontcontroller');
+        $router = $this->getResource('frontcontroller')->getRouter();
 
+        $pages = array(
+            'about', 'help', 'terms-of-service',
+        );
+        foreach ($pages as $page) {
+            $router->addRoute($page,
+                new Zend_Controller_Router_Route_Static($page, array(
+                    'controller' => 'index',
+                    'action' => 'page',
+                    'slug' => $page,
+                )));
+        }
+    }
+
+    /**
+     * The second method, and one I'm not a particular fan of, uses a standard
+     * route... but only one of them. There are two obvious advantages to this:
+     *
+     *  1) You only have one route to mess with.
+     *  2) You don't need to manage a list of pages; it all falls to the
+     *     IndexController->pageAction() method.
+     *
+     * Then again, having the slug at root-level is no longer feasible, so you'd
+     * have to prepend it with something, like in this example... "page". From
+     * experience, if you have a digital strategist on your team, they might
+     * throw a fit because they don't get complete control over of the URL.
+     */
+//    protected function _initMvcStandardRoutes()
+//    {
+//        $this->bootstrap('frontcontroller');
+//        $router = $this->getResource('frontcontroller')->getRouter();
+//
+//        $router->addRoute('page',
+//            new Zend_Controller_Router_Route('page/:slug', array(
+//                'controller' => 'index',
+//                'action' => 'page',
+//            )));
+//    }
 
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -1,18 +1,42 @@
 <?php
-
 class IndexController extends Zend_Controller_Action
 {
-
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
-
     public function indexAction()
     {
-        // action body
     }
+    
+    public function pageAction()
+    {
+        if ($this->_hasParam('slug')) {
+            // If we get a slug param, then it's time to go searching for our
+            // static page. We wrap it in a try/catch and do a simple check for
+            // whether or not the view script was found. If it wasn't, then we
+            // just ignore the exception and send the user onto the error_handler
+            try {
+                $page = strtolower($this->_getParam('slug'));
+                $this->render($page);
+                return;
+            } catch (Zend_View_Exception $e) {
+                // Simple check for the view being unable to find the page that
+                // is being requested. If we don't have a match, then throw the
+                // original exception.
+                if (false === strpos($e->getMessage(), 'not found in path')) {
+                    throw $e;
+                }
+            }
+        }
 
-
+        // We're artificially invoking an Exception here so that the error
+        // controller can handle it as it wants. It's almost too easy.
+        $error = new stdClass();
+        $error->type = Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION;
+        $error->exception = new Zend_Controller_Action_Exception(
+                'The page you are looking for does not exist', 404);
+        $error->request = clone $this->getRequest();
+        
+        $this->getRequest()->setParam('error_handler', $error);
+        
+        $this->_forward('error', 'error');
+    }
 }
 
